68943ad0f585cb6564299694e212e0478bb9379c
Tim Supan <timsupann@gmail.com>
2023-05-03 16:05:14 -0700

    Add for loop to repeat string to desired amount

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let msg = "";
+    for (let i = 0; i < num; i++){
+        msg += string;
+    }
+    return msg;
 };
 
 // Do not edit below this line
