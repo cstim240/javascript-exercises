fbb5154f883c546e531ad925ee145e64dd2a3f6d
Tim Supan <timsupann@gmail.com>
2023-05-03 16:29:38 -0700

    Fill in function to reverse string args

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(string, num) {
     let msg = "";
+    if (num < 0){
+        return "Error!";
+    }
     for (let i = 0; i < num; i++){
         msg += string;
     }
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    for (let i = string.length-1; i >=0; i--){
+        reversedString += string[i]; //or string.charAt(i)
+    }
+    return reversedString;
 
+    
 };
+
+//or return string.split("").reverse().join("");
 
 // Do not edit below this line
 module.exports = reverseString;
