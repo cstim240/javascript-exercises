ae3a1e1270898fa3a41131e245a80b50324847df
Tim Supan <timsupann@gmail.com>
2023-05-04 00:36:04 -0700

    Change all tests.skip to test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function(string, num) {
     let msg = "";
     if (num < 0){
-        return "Error!";
+        return "ERROR";
     }
     for (let i = 0; i < num; i++){
         msg += string;
