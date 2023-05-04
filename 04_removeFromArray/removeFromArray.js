fa4ebc14112bc27b185c2696ad969c54271d27bf
Tim Supan <timsupann@gmail.com>
2023-05-03 17:58:05 -0700

    Create a function which removes specific element(s) from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...target) {
+    const newArr = [];
 
-};
+    for (let i = 0; i < array.length; i++){
+        if (target.includes(array[i])){
+            continue;
+        } else {
+            newArr.push(array[i]);
+        }
+    }
+
+    return newArr;
+
+}
+
+/**
+ * if (target.length > 1){
+        for (let i = 0; i < target.length; i++){
+            let targetIndex = array.indexOf(target[i]);
+            newArr = array.splice(targetIndex, targetIndex);
+            return newArr;
+        }
+    } 
+ */
 
 // Do not edit below this line
 module.exports = removeFromArray;
