89429d603f99685c473c521e89b764568bd76149
Tim Supan <timsupann@gmail.com>
2023-05-16 12:39:18 -0700

    Create multipl and sum functions, edit spec js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,12 +6,12 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) { //want sum of an array, use .reduce()
+  return array.reduce((total, item) => total + item, 0); //the 0 is the initial value of the total variable
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, element) => product * element, 1);
 };
 
 const power = function() {
