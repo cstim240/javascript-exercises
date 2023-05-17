ee72dfa7c6be003be82a230c0014e12f103e81db
Tim Supan <timsupann@gmail.com>
2023-05-17 00:35:50 -0700

    Create findtheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array, person) {
+    let currentOldest = person;
+    let age = 0;
+    array.forEach((person) => {
+        if (!person.hasOwnProperty('yearOfDeath')){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        let ageInQuestion = person.yearOfDeath - person.yearOfBirth;
+        if (ageInQuestion > age){
+            currentOldest = person;
+            console.log(currentOldest);
+            age = ageInQuestion;
+        }
+    });
+    return currentOldest;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
